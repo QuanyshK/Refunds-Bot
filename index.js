39d3211dff3d40df527dc7f845cb45864ae6cf91
QuanyshK <zoomer.qurbany@gmail.com>
2024-11-14 01:29:13 +0500

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ bot.on('text', (ctx) => {
     ctx.reply('Сообщение получено!');
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
